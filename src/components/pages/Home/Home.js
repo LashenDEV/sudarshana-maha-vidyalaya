7c2d60484b0d1291bdc2cf565773ef264a3eea00
LashenDEV <lashenkari@gmail.com>
2022-05-23 15:25:53 +0530

    init

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="container">
+            <div className="card shadow">
+                <div className="card-body">
+                    <h2>Home Page</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
