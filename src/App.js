0b5ea3a024b598f389a0f8c37f32d5643efbed85
LashenDEV <lashenkari@gmail.com>
2022-06-04 22:32:43 +0530

    ✔️Navbar done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Home from './components/pages/Home/Home'
 import About from './components/pages/About/About'
+import History from "./components/pages/History/History";
 import Contact from './components/pages/Contact/Contact'
+import Navbar from "./components/includes/Navbar";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Gallery from "./components/pages/Gallery/gallery";
 
 function App() {
     return (
-        <div>
-            <Home />
-            <About />
-            <Contact />
-        </div>
+        <Router>
+            <div>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/contact" element={<Contact/>} />
+                    <Route path="/history"element={<History/>} ></Route>
+                    <Route path="/gallery"element={<Gallery/>} ></Route>
+                </Routes>
+
+            </div>
+        </Router>
     );
 }
 
---
+++ src/components/includes/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link to='/' className="navbar-brand d-flex align-items-center">
+                    <img
+                        src="https://www.kindpng.com/picc/m/7-70674_thailand-school-logo-school-logos-free-download-hd.png"
+                        alt="" width="50" height="44"/>
+                    <h3 className="ps-2 d-none d-md-block m-0">Sudarshana Maha Vidyalaya</h3></Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul className="navbar-nav mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to='/' className="nav-link active">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/about' className="nav-link active">News And Events</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/history' className="nav-link active">History</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/gallery' className="nav-link active">Gallery</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/contact' className="nav-link active">Contact Us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
