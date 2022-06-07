55ad61e71a94a48ebcc4b5ba4093b76e226cc795
ChandanaEkanayaka <jagath99t@gmail.com>
2022-06-07 23:16:52 +0530

    ✅ about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,15 +1,20 @@
 import React from "react";
+import "./About.css"
 
 function About() {
     return (
-        <div className="container">
-            <div className="card shadow">
-                <div className="card-body">
-                    <h2>About Page</h2>
+        
+        <div class="card mb-3">
+            <img src="../About/img/misson.jpg" class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <h5 class="card-title"> Our Misson </h5>
+                    <p class="card-text">"Creating a student generation with a full-blown humanitarian, balanced personality that can succeed in creative thinking and talent."</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
-            </div>
-        </div>
+         </div>
+        
     )
+    
 }
 
 export default About;
