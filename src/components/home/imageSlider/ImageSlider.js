9660151340e0b4577598a8b0b893e5fddbe74404
Lashen Kariyawasam <56374597+LashenKariyawasam@users.noreply.github.com>
2022-06-19 14:41:42 +0530

    🐛 image carousel bug fixed with preloader

--- src/components/home/imageSlider/ImageSlider.js
+++ src/components/home/imageSlider/ImageSlider.js
@@ -1,51 +1,52 @@
 import React from "react";
+import {Carousel} from "react-bootstrap";
 
 function ImageSlider() {
     return (
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active"
-                        aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                        aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                        aria-label="Slide 3"></button>
-            </div>
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src="https://media-exp1.licdn.com/dms/image/C561BAQEoxx7Fst73qQ/company-background_10000/0/1593404614510?e=2147483647&v=beta&t=wai5xGV5X-XAjn0c1FPVvZiyrEKNG9LJWrS2m-kuRig" className="d-block w-100" alt="..."/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>First slide label</h5>
-                            <p>Some representative placeholder content for the first slide.</p>
-                        </div>
-                </div>
-                <div className="carousel-item">
-                    <img src="https://www.dailynews.lk/sites/default/files/news/2020/02/17/z_p26-Siridhamma.jpg" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Second slide label</h5>
-                            <p>Some representative placeholder content for the second slide.</p>
-                        </div>
-                </div>
-                <div className="carousel-item">
-                    <img src="https://www.worldbank.org/content/dam/photos/780x439/2016/mar-7/lk-tsep-assembly.jpg" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>Third slide label</h5>
-                            <p>Some representative placeholder content for the third slide.</p>
-                        </div>
-                </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions"
-                    data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions"
-                    data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+        <div>
+            <Carousel>
+                <Carousel.Item style={{height: "500px"}}>
+                    <img
+                        className="d-block w-100"
+                        src="https://imgs.search.brave.com/c6AZCAXnreG5cf6wXtkHliA0m6yiDpA0Rdedzcmc6ho/rs:fit:1200:600:1/g:ce/aHR0cHM6Ly9pbmR1/c3Njcm9sbHMuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDIw/LzA2LzYwNTkxMTQw/XzM2MjMzNTgwMTA2/NzM5NF8xMzc4OTY0/NDM1MDk5NDUxMzky/X24uanBn"
+                        alt="First slide"
+                        style={{ width: "100%", objectFit: "cover" }}
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item style={{height: "500px"}}>
+                    <img
+                        className="d-block w-100"
+                        src="https://imgs.search.brave.com/kK_bIlTZ2b8J3Ax2CVtmZeacSbhE1LfZMGgemH6bXl0/rs:fit:1024:576:1/g:ce/aHR0cHM6Ly93d3cu/YWRiLm9yZy9zaXRl/cy9kZWZhdWx0L2Zp/bGVzL3N0eWxlcy9j/b250ZW50X21lZGlh/L3B1YmxpYy9jb250/ZW50LW1lZGlhLzY2/NzMtc2Vjb25kYXJ5/LXN0dWRlbnRzLWxl/YXZpbmctbmV3LWxh/Yi5qcGc_aXRvaz16/aTdqOEdGcg"
+                        alt="Second slide"
+                        style={{ width: "100%", objectFit: "cover" }}
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item style={{height: "500px"}}>
+                    <img
+                        className="d-block w-100"
+                        src="https://imgs.search.brave.com/id1OBhk0OS3xF9FAVE85k1p7zNfp5k9XolP89Ycl_7o/rs:fit:1200:1200:1/g:ce/aHR0cDovL3d3dy5y/ZWRjcm9zcy5say93/cC1jb250ZW50L3Vw/bG9hZHMvMjAxNy8w/OC9JTUdfNzEwMS5q/cGc"
+                        alt="Third slide"
+                        style={{ width: "100%", objectFit: "cover" }}
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
+
 }
 
 export default ImageSlider;
