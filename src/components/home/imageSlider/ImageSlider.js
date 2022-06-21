047a90d17067613735a7d6f2b93209724a098002
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-06-21 15:13:45 +0530

    🎨 code refactored

--- src/components/home/imageSlider/ImageSlider.js
+++ src/components/home/imageSlider/ImageSlider.js
@@ -3,16 +3,12 @@ import {Carousel} from "react-bootstrap";
 
 function importAll(r) {
     let images = {};
-    r.keys().forEach((item, index) => {
+    r.keys().forEach((item) => {
         images[item.replace('./', '')] = r(item);
     });
     return images
 }
-
-
 const SliderImages = importAll(require.context('../../images/image slider/', false, /\.(png|jpe?g|svg)$/));
-
-console.log(SliderImages)
 
 function ImageSlider() {
     return (
