7064ac82101b02369bbc39bcf3a099478c69e8b6
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-06-26 14:28:34 +0530

    🐛 bug fixed

--- src/components/Animations/ScrollToTop/ScrollToTop.js
+++ src/components/Animations/ScrollToTop/ScrollToTop.js
@@ -3,6 +3,7 @@ import {useLocation} from "react-router-dom";
 
 function ScrollToTop({history}) {
     const routePath = useLocation();
+    console.log(routePath);
     const onTop = () => {
         window.scrollTo(0, 0);
     }
