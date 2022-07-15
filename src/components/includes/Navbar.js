60461d8187b880e68aa9a2e2be9edfd68ad669e3
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-07-15 20:50:41 +0530

    🎨 navbar active/hover done

--- src/components/includes/Navbar.js
+++ src/components/includes/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {Link, useMatch, useResolvedPath} from 'react-router-dom';
+import "./Navbar.css";
 import Crest from '../images/logo/crest.png';
 
 function Navbar() {
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid ms-5 my-3">
@@ -11,35 +13,45 @@ function Navbar() {
                         src={Crest}
                         alt="" width="60" height="60"/>
                     <h3 className="ps-2 d-none d-md-block m-0">Sudarshana Maha Vidyalaya</h3></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                <a className="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>
+                </a>
                 <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                     <ul className="navbar-nav mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link to='/sudarshana-maha-vidyalaya/' className="nav-link active">Home</Link>
+                            <NavLink to='/sudarshana-maha-vidyalaya/'>Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/sudarshana-maha-vidyalaya/about' className="nav-link active">About</Link>
+                            <NavLink to='/sudarshana-maha-vidyalaya/about'>About</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/sudarshana-maha-vidyalaya/news_events' className="nav-link active">News & Events</Link>
+                            <NavLink to='/sudarshana-maha-vidyalaya/news_events'>News &
+                                Events</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/sudarshana-maha-vidyalaya/history' className="nav-link active">History</Link>
+                            <NavLink to='/sudarshana-maha-vidyalaya/history'>History</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/sudarshana-maha-vidyalaya/gallery' className="nav-link active">Gallery</Link>
+                            <NavLink to='/sudarshana-maha-vidyalaya/gallery'>Gallery</NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link to='/sudarshana-maha-vidyalaya/contact' className="nav-link active">Contact Us</Link>
+                            <NavLink to='/sudarshana-maha-vidyalaya/contact'>Contact Us</NavLink>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
+    )
+}
+
+
+function NavLink({to, children, ...props}) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end: true})
+    return (
+        <Link to={to} className={isActive ? 'nav-link active' : 'nav-link '} {...props}>{children}</Link>
     )
 }
 
